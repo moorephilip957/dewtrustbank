4b30b23ede49a3ef37074b5083580eaaa257fd70
NephygTdkingz <tdkingzict@gmail.com>
2026-05-06 10:29:23 +0100

    finished the home page part

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,11 +2,18 @@
 from django.urls import path
 from . import views
 
-app_name = 'frontend'  # this is the namespace
+app_name = 'frontend'  
 
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('personal/', views.personal_view, name='personal'),
     path('corperate/', views.corperate_view, name='corperate'),
     path('insurance/', views.insurance_view, name='insurance'),
+    path('mortgages/', views.mortgages_view, name='mortgages'),
+    path('savings/', views.savings_view, name='savings'),
+    path('loans/', views.loans_view, name='loans'),
+    path('cards/', views.cards_view, name='cards'),
+    path('about/', views.about_view, name='about'),
+    path('contact/', views.contact_view, name='contact'),
+    path('terms/', views.terms_view, name='terms'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -11,3 +11,24 @@ def corperate_view(request):
 
 def insurance_view(request):
     return render(request, 'frontend/insurance.html')
+
+def mortgages_view(request):
+    return render(request, 'frontend/mortgages.html')
+
+def savings_view(request):
+    return render(request, 'frontend/seftons/savings.html')
+
+def loans_view(request):
+    return render(request, 'frontend/seftons/loans.html')
+
+def cards_view(request):
+    return render(request, 'frontend/seftons/cards.html')
+
+def about_view(request):
+    return render(request, 'frontend/seftons/about.html')
+
+def contact_view(request):
+    return render(request, 'frontend/seftons/contact.html')
+
+def terms_view(request):
+    return render(request, 'frontend/seftons/terms.html')
